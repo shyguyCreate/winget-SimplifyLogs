b5b03cbce6f314bc2e8ab8b88808b772c1baaee2
shyguyCreate <107062289+shyguyCreate@users.noreply.github.com>
2022-07-04 12:18:35 -0500

    Readded debug to function

--- wingetSimplifyLogs.ps1
+++ wingetSimplifyLogs.ps1
@@ -76,6 +76,76 @@ function Get-FileListVerbose ([string] $logPathParam, [string] $logMatch)
     return $logFileVerbose;
 }
 
+#ONLY for debug
+function Debug-NumberOfMatches ([array] $logArray)
+{
+    #Debug variables
+    $Stepping_statement = 0;        $_arrow_ = 0
+    $Statement_has_completed = 0;   $SELECT_WHERE = 0
+    $Statement_has_data = 0;        $INSERT = 0
+    $_savepoint = 0;                $SELECT_WHERE_ = 0
+    $SAVEPOINT = 0;                 $SELECT_COUNT = 0
+    $ROLLBACK = 0;                  $UPDATE = 0
+    $RELEASE = 0;                   $DELETE = 0
+    $_TABLE = 0;                    $ORDER_BY = 0
+    $Reset_statement = 0;           $select_value = 0
+    $_INDEX = 0;
+    $Setting_action = 0;
+
+    #Here the matches will be count.
+    for ($i = 0; $i -lt $logArray.Length; $i++) 
+    {
+        #First part
+        if ($logArray[$i] -match '^Stepping statement #\d+$') {  $Stepping_statement++;  continue; }
+        if ($logArray[$i] -match '^Statement #\d+ has completed$') {  $Statement_has_completed++;  continue; }
+        if ($logArray[$i] -match '^Statement #\d+ has data$') {  $Statement_has_data++;  continue; }
+        if ($logArray[$i] -match '^(\w+ )+savepoint:') {  $_savepoint++;  continue; }
+        if ($logArray[$i].StartsWith('SAVEPOINT')) {  $SAVEPOINT++;  continue; }
+        if ($logArray[$i].StartsWith('ROLLBACK')) {  $ROLLBACK++;  continue; }
+        if ($logArray[$i].StartsWith('RELEASE')) {  $RELEASE++;  continue; }
+        if ($logArray[$i] -cmatch '^(\w+ )+TABLE') {  $_TABLE++;  continue; }
+        if ($logArray[$i] -match '^Reset statement #\d+$') {  $Reset_statement++;  continue; }
+        if ($logArray[$i] -cmatch '^(\w+ )+INDEX') {  $_INDEX++;  continue; }
+        if ($logArray[$i].StartsWith('Setting action:')) {  $Setting_action++;  continue; }
+        
+        #Second part
+        if ($logArray[$i] -cmatch '^\d+ =>') {  $_arrow_++; continue; }        
+        if ($logArray[$i] -cmatch '^SELECT \[.+WHERE (\[\w+\]) ') {  $SELECT_WHERE++; continue; }
+        if ($logArray[$i] -match '^INSERT') {  $INSERT++; continue; }
+        if ($logArray[$i].StartsWith('SELECT COUNT')) {  $SELECT_COUNT++; continue; }
+        if ($logArray[$i] -cmatch '^SELECT \[.+WHERE (\[\w+\])\.') {  $SELECT_WHERE_++; continue; }        
+        if ($logArray[$i].StartsWith('UPDATE')) {  $UPDATE++; continue; }
+        if ($logArray[$i].StartsWith('DELETE')) {  $DELETE++; continue; }        
+        if ($logArray[$i].EndsWith('ORDER BY [t].[sort]')) {  $ORDER_BY++; continue; }        
+        if ($logArray[$i].StartsWith('select [value]')) {  $select_value++; continue; }
+    }
+
+    Write-Output "
+    `r`$Stepping_statement: $Stepping_statement
+    `r`$Statement_has_completed: $Statement_has_completed
+    `r`$Statement_has_data: $Statement_has_data
+    `r`$_savepoint: $_savepoint
+    `r`$SAVEPOINT: $SAVEPOINT
+    `r`$ROLLBACK: $ROLLBACK
+    `r`$RELEASE: $RELEASE
+    `r`$_TABLE: $_TABLE
+    `r`$Reset_statement: $Reset_statement
+    `r`$_INDEX: $_INDEX
+    `r`$Setting_action: $Setting_action
+
+    `r`$_arrow_: $_arrow_
+    `r`$SELECT_WHERE: $SELECT_WHERE
+    `r`$INSERT: $INSERT
+    `r`$SELECT_COUNT: $SELECT_COUNT
+    `r`$SELECT_WHERE_: $SELECT_WHERE_
+    `r`$UPDATE: $UPDATE
+    `r`$DELETE: $DELETE
+    `r`$ORDER_BY: $ORDER_BY
+    `r`$select_value: $select_value
+    "
+}
+
+
 function Format-Log ([array] $logArray)
 {
     [array] $logArrayReturn = @();
@@ -97,7 +167,6 @@ function Format-Log ([array] $logArray)
         if ($logArray[$i] -cmatch '^(\w+ )+INDEX') { continue; }
         if ($logArray[$i].StartsWith('Setting action:')) { continue; }
         
-        
         #Here is the fun part where a lot of data will not get to the final file, some will be joined with another line
         #to generate less lines overall and some will be replaced by shorted version from their original lines.
         #Comment lines at your consideration.
@@ -108,10 +177,10 @@ function Format-Log ([array] $logArray)
             continue; }
         if ($logArray[$i] -match '^INSERT') { 
             continue; }
-        if ($logArray[$i] -cmatch '^SELECT \[.+WHERE (\[\w+\])\.') { 
-            continue; }        
         if ($logArray[$i].StartsWith('SELECT COUNT')) { 
             continue; }
+        if ($logArray[$i] -cmatch '^SELECT \[.+WHERE (\[\w+\])\.') { 
+            continue; }        
         if ($logArray[$i].StartsWith('UPDATE')) {
             $logArrayReturn += $logArray[$i] -replace '^UPDATE.+SET (\[\w+\]).+',"`$1 $($logArray[($i+1)]-replace '^\d+ =>','=')" ;
             continue; }
@@ -149,6 +218,9 @@ do{
 
 #This will get all the log lines and will delete extra information established inside the function.
 [array] $logContent = Get-LogContent-And-Clear-ExtraInfo -Path $logFile;
+
+#Uncomment this line for counting the number of matches produced.
+# Debug-NumberOfMatches -logArray $logContent
 
 #Here many of the lines will be removed or change to make the log file more understandable.
 $logContent = Format-Log $logContent;
